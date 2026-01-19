753112c3d61e69013a0534888b6ede5439418d31
HotaruOs <arroztlg@gmail.com>
2026-01-19 10:36:58 -0300

    AxionOs bringUp

--- lineage_tundra.mk
+++ lineage_tundra.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/tundra/device.mk)
 
 # Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_tundra
@@ -26,3 +27,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tundra_g-user 14 U1SJS34.2-92-10-9 40971b-55161 release-keys" \
     BuildFingerprint=motorola/tundra_g/tundra:14/U1SJS34.2-92-10-9/40971b-55161:user/release-keys \
     DeviceProduct=tundra_g
+
+TARGET_ENABLE_BLUR := true
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,13,2
+AXION_CAMERA_FRONT_INFO := 32
+
+# Maintainer/Soc name
+AXION_MAINTAINER := HotaruOs
+AXION_PROCESSOR := Snapdragon_888+
+
+# Enable or disable ScrollOptimizer globally
+persist.sys.perf.scroll_opt = true
+persist.sys.perf.scroll_opt.heavy_app = 0
+
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
